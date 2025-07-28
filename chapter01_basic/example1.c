2ee11b9a19352b796601e4916e2ea6114d3059e2
manojm33 <141601901+manojm33@users.noreply.github.com>
2025-07-28 20:10:08 +0530

    Add files via upload

---
+++ chapter01_basic/example1.c
@@ -0,0 +1,13 @@
+/* First C pogram
+ * This program in written tho print the words "Hello World" on to the screennnnnn
+ * Author: manoj
+ * Date: 28 Jul 2025
+ */
+
+#include<stdio.h>
+
+int main(void)
+{
+	printf("Hello World");
+	return 0;
+}
