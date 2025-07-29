ac37a144b8c15f0a415ae5a9ebe30dc91dfdc19c
manojm33 <141601901+manojm33@users.noreply.github.com>
2025-07-29 22:08:12 +0530

    Add files via upload

---
+++ chapter01_basic/example2.c
@@ -0,0 +1,18 @@
+/* Example 2
+ * Write a program to convert kilometeres into miles
+ * Date: 28 Jul 2025
+ * Author: Manoj
+ */
+
+#include<stdio.h>
+
+int main()
+{
+	float km, miles;
+	miles = 10.0;
+	km = miles * 1.609;
+	printf("%f mile equals %f kilometer\n", miles, km);
+	return 0;
+}
+
+
